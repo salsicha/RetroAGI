99779714b3ed85b9a79a25569d8b1910c0ca35ae
Alex Moran <alex.moran@supernal.aero>
2024-11-30 16:19:13 -0800

    script placeholder

---
+++ main.py
@@ -0,0 +1,31 @@
+
+
+# get constraints
+
+
+# create constraints
+
+
+# start playthrough
+
+
+# predict next token
+
+
+# search
+
+
+# end play through
+
+
+# cluster
+
+
+# build model
+
+
+# record tokens
+
+
+# train
+
