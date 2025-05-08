e63eaa591d2b299efa46127db1e1b9d91f683690
Alex Moran <salsicha@gmail.com>
2025-05-08 13:13:09 -0700

    added motif freq counting

--- scripts/motifs.py
+++ scripts/motifs.py
@@ -2,33 +2,56 @@ import stumpy
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+from numba import cuda
 
-plt.rcParams["figure.figsize"] = [20, 6]  # width, height
-plt.rcParams['xtick.direction'] = 'out'
 
-m = 24
-c = np.random.rand(10000)
-p = stumpy.stump(c, m)
+class Motifs():
+    def __init__(self):
+        self.max_motifs = 2
+        self.m = 24
+        self.c = np.random.rand(10000)
+        self.p = stumpy.stump(self.c, self.m)
 
-dists, inde = stumpy.motifs(c, p[:, 0], max_motifs=2)
+        all_gpu_devices = [device.id for device in cuda.list_devices()]
+        p = stumpy.gpu_stump(self.c, self.m, device_id=all_gpu_devices)
 
-fig, axs = plt.subplots(2, sharex=True, gridspec_kw={'hspace': 0})
-plt.suptitle('Motif (Pattern) Discovery', fontsize='30')
-axs[0].plot(c)
-axs[0].set_ylabel('CO2 ppm', fontsize='20')
+        self.dists, self.inde = stumpy.motifs(self.c, self.p[:, 0], max_motifs=self.max_motifs)
 
-cols = ['red' , 'green', 'blue' ]
+        # fig, axs = plt.subplots(2, sharex=True, gridspec_kw={'hspace': 0})
+        # plt.suptitle('Motif (Pattern) Discovery', fontsize='30')
+        # axs[0].plot(c)
+        # axs[0].set_ylabel('CO2 ppm', fontsize='20')
 
-for z in range(0, inde.shape[0]):
-    col = cols[z]
-    start = inde[z, 0]
-    stop = inde[z, 0] + m
-    matches = stumpy.match(c[start:stop],c, max_distance=2.0) 
-    for mt in range(matches.shape[0]):
-        s = matches[mt, 1]
-        st = s + m
-        axs[0].plot(np.arange(s, st), c[s : st], c=col)
+        self.cols = ['red' , 'green', 'blue' ]
 
-axs[1].plot(p[:, 0])
-axs[1].set_ylabel('Matrix profile', fontsize='20')
-plt.show()
+    def find_motifs(self):
+
+        match_count = {}
+        i = 0
+
+        for z in range(0, self.inde.shape[0]):
+            col = self.cols[z]
+            start = self.inde[z, 0]
+            stop = self.inde[z, 0] + self.m
+            matches = stumpy.match(self.c[start:stop], self.c, max_distance=2.0)
+            match_count[i] = matches.shape[0]
+            i += 1
+
+            for mt in range(matches.shape[0]):
+                s = matches[mt, 1]
+                st = s + self.m
+                # axs[0].plot(np.arange(s, st), c[s : st], c=col)
+
+        # axs[1].plot(p[:, 0])
+        # axs[1].set_ylabel('Matrix profile', fontsize='20')
+        # plt.show()
+
+        # TODO: calculate frequency of motifs, return the most frequent ones
+
+        sorted_matches = sorted(match_count.items(), key=lambda x: x[1], reverse=True)
+        most_frequent_motifs = sorted_matches[:self.max_motifs]
+        most_frequent_motifs = {k: v for k, v in most_frequent_motifs}
+        # print(most_frequent_motifs)
+
+        return self.inde
+    
